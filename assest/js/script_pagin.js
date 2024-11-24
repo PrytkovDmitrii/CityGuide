f1b033e0e827ef9b7a90a89e431540d8e623c6fc
PrytkovDmitrii <prytkovdima53@gmail.com>
2024-11-24 22:36:22 +0300

    Sorting and pagination

--- assest/js/script_pagin.js
+++ assest/js/script_pagin.js
@@ -1,8 +1,6 @@
-const filter = document.querySelectorAll('.box');
-
-document.querySelector('.attractions__pagination').addEventListener('click', event => {
-    
+document.querySelector('.attractions__pagination').addEventListener('click', event => { 
     let filterClass = event.target.dataset['f'];
+    const filter = document.querySelectorAll('.box');
     
     filter.forEach(elem => {
         elem.classList.remove('hide')
